28f18b146c6bfd1b332485225c4dd65b7c950bf7
Sam <sam@expertcoder.io>
2019-01-14 12:03:34 +1100

    test fixtures can now generate uuid4

--- tests/Fixtures.php
+++ tests/Fixtures.php
@@ -4,8 +4,18 @@ declare(strict_types = 1);
 
 namespace Target365\ApiSdk\Tests;
 
+use Ramsey\Uuid\Uuid;
+
 class Fixtures
 {
+    static public function generateUuid4(): string
+    {
+        $uuid4 = Uuid::uuid4();
+
+        return $uuid4->toString(); // i.e. 25769c6c-d34d-4bfe-ba98-e0ee856f3e7a
+    }
+
+
     static public function getShortNumberId()
     {
         return 'NO-0000';
@@ -14,6 +24,19 @@ class Fixtures
     static public function getTransactionId()
     {
         return '79f35793-6d70-423c-a7f7-ae9fb1024f3b';
+    }
+
+
+    static private $fixedRandomTransactionId = null;
+    static public function getFixedRandomTransactionId(): string
+    {
+        if (self::$fixedRandomTransactionId) {
+            return self::$fixedRandomTransactionId;
+        } else {
+            self::$fixedRandomTransactionId = self::generateUuid4();
+
+            return self::$fixedRandomTransactionId;
+        }
     }
 
 }
